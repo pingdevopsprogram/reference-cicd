302776e4a25d4114b3a8a14351781634ae685fd7
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-11-04 16:03:42 -0600

    building travis-ci

---
+++ ci-scripts/install-dependencies.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" unzip awscliv2.zip
+
+./aws/install && apt install amazon-ecr-credential-helper -y
