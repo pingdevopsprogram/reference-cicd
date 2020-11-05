f3f633e4ea39a60250fcc38d34f290f8aa4094b4
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-11-05 00:22:50 -0600

    typo in generate secret

--- ci-scripts/check-tools.sh
+++ ci-scripts/check-tools.sh
@@ -7,4 +7,4 @@ kubectl get all
 helm repo ls
 helm ls
 
-kubectl create secret generic devops --from-literal=PING_IDENTITY_DEVOPS_USER="${PING_IDENTITY_DEVOPS_USER}" --from-literal=PING_IDENTITY_DEVOPS_KEY="${PING_IDENTITY_DEVOPS_KEY}"
+kubectl create secret generic devops-secret --from-literal=PING_IDENTITY_DEVOPS_USER="${PING_IDENTITY_DEVOPS_USER}" --from-literal=PING_IDENTITY_DEVOPS_KEY="${PING_IDENTITY_DEVOPS_KEY}"
