0ceda65effcd5b6d49c09a0ae095a59e85d86c17
pingdevopsprogram <53316629+pingdevopsprogram@users.noreply.github.com>
2020-12-04 14:27:46 -0600

    GitHub actions (#4)
    * latest ubuntu
    
    * need to sudo install
    
    * fix version command
    
    * go verbose
    
    * try go for ecs
    
    * just one version
    
    * fixed kubeconfig
    
    * go is no-go
    
    * install aws cli
    
    * aws iam auth and helm
    
    * organize
    
    * organize
    
    * fix script name
    
    * actually need to execute the script of course
    
    * add travis vars image
    
    * change path
    
    * have to apt-get update for helm
    
    * try a helm release
    
    * multiple stages is slow
    
    * typo in generate secret
    
    * streamline
    
    * postman test
    
    * break integration test with bad profile
    
    * back to baseline,fix integration test
    
    * get latest baseline
    
    * touch file to change sha
    
    * upgrade helm chart to 3.2
    
    * try github actions
    
    * new helm repo
    
    * some update
    
    * added secrets
    
    * fix file name typi
    
    * move script code directly to workflow
    
    * mv kubectl check
    
    * move more
    
    * try kubectl action
    
    * add aws iam authn
    
    * Update aws.yml
    
    * Update aws.yml
    
    * Update aws.yml
    
    * Update aws.yml
    
    * Update aws.yml
    
    * Update aws.yml
    
    * add helm
    
    * Update aws.yml
    
    * try deploy from yaml
    
    * wait-for pd
    
    * add echo
    
    * need to ignore  not found
    
    * move helm deploy to script
    
    * typo
    
    * cut github ref
    
    * added new message
    
    * clean profile
    
    * try to fix why profile sha
    
    * trigger workflow
    
    * remove uses key
    
    * show erik
    
    Co-authored-by: samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>

--- ci-scripts/install-dependencies.sh
+++ ci-scripts/install-dependencies.sh
@@ -10,7 +10,7 @@ sudo ./aws/install
 # INSTALL aws-iam-authenticator
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.8/2020-09-18/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
-mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && echo PATH=$PATH:$HOME/bin
 aws-iam-authenticator help
 
 if test -n "$AWS_CONFIG" ; then
@@ -38,7 +38,7 @@ sudo apt-get install apt-transport-https --yes
 echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get -y install helm
-helm repo add pingidentity https://helm.pingidentity.com/devops
+helm repo add pingidentity https://helm.pingidentity.com/
 helm repo update
 
 
