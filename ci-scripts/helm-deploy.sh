79acca1786fc36bc9bd83c1b9026789ad61d04c3
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-12-06 23:13:49 -0600

    working on helm rollback

--- ci-scripts/helm-deploy.sh
+++ ci-scripts/helm-deploy.sh
@@ -2,34 +2,28 @@
 
 set -x
 
+_ref=$(git rev-parse --abbrev-ref HEAD)
+echo _ref
+
 ## builds sha for each product based on the folder name in ./profiles/* (e.g. pingfederateSha)
 for D in ./profiles/* ; do 
   if [ -d "${D}" ]; then 
     _prodName=$(basename "${D}")
     dirr="${D}"
-    echo "${D}"
-    eval echo "${_prodName}Sha"
-    sleep 1
-    git rev-list -n 1 --no-merges --all  -- "$dirr"
-    sleep 1
-    git --no-pager log -n 1 --pretty=format:%h -- "$dirr"
-    sleep 1
-    git --no-pager log -n 1 HEAD --pretty=format:%h -- "$dirr"
-    sleep 1
-    git --no-pager log -n 1 restart-helm --pretty=format:%h -- "$dirr"
-    sleep 1
-    eval "${_prodName}Sha=$(git rev-list -n 1 --no-merges --all  -- "$dirr" | cut -c 1-8)"
+    eval "${_prodName}Sha=$(git log -n 1 --pretty=format:%h -- "$dirr")"
   fi
 done
 
-git --version
 # # install the new profiles, but don't move on until install is successfully deployed. 
 # # tied to chart version to avoid breaking changes.
-# helm upgrade --install \
-#   sg-dev-fs pingidentity/ping-devops \
-#   --set pingfederate-admin.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
-#   --set pingfederate-engine.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
-#   --set pingdirectory.envs.SERVER_PROFILE_SHA="${pingdirectorySha}" \
-#   --set global.envs.SERVER_PROFILE_BRANCH="$(echo ${GITHUB_REF} | cut -b 12-)" \
-#   -f helm/dev-values.yaml \
-#   --wait --timeout 10m0s
+##TODO - turn this into a loop, 1 deploy for each product, so it's easier to rollback failures.  
+helm upgrade --install \
+  "${_pingEnv}" pingidentity/ping-devops \
+  --set pingfederate-admin.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
+  --set pingfederate-engine.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
+  --set pingdirectory.envs.SERVER_PROFILE_SHA="${pingdirectorySha}" \
+  --set global.envs.SERVER_PROFILE_BRANCH="${_ref}" \
+  -f helm/dev-values.yaml \
+  --atomic --timeout 10m0s
+
+helm history "${_pingEnv}"
