518f2d5912eea3f767719e1809546e7df8940ae0
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-12-08 15:33:40 -0600

    back to deploying all at once

--- ci-scripts/helm-deploy.sh
+++ ci-scripts/helm-deploy.sh
@@ -4,9 +4,11 @@ set -x -e
 
 _ref=$(git rev-parse --abbrev-ref HEAD)
 PING_ENV=${PING_ENV:=sg-dev-fs}
-echo _ref
+echo ${_ref}
+_currentSha=$(git log -n 1 --pretty=format:%h)
 
 ## builds sha for each product based on the folder name in ./profiles/* (e.g. pingfederateSha)
+  ## this determines what will be redeployed. 
 for D in ./profiles/* ; do 
   if [ -d "${D}" ]; then 
     _prodName=$(basename "${D}")
@@ -15,38 +17,20 @@ for D in ./profiles/* ; do
   fi
 done
 
+#try to minimize extended crashloops
+_timeout="2m0s"
+test "${pingdirectorySha}" = "${_currentSha}" && _timeout="10m0s"
+
 # # install the new profiles, but don't move on until install is successfully deployed. 
 # # tied to chart version to avoid breaking changes.
-##TODO - turn this into a loop, 1 deploy for each product, so it's easier to rollback failures.  
-
-for _prodName in  pingdirectory pingfederate_admin pingfederate; do
-  echo "$_prodName Sha is:" "$(eval "echo \${${_prodName}Sha}")"
-  case ${_prodName} in
-    pingdirectory)
-      timeout="10m0s"
-      _helmVars="--set pingdirectory.envs.PD_PROFILE_SHA=${pingdirectorySha}"
-      ;;
-    pingfederate_admin)
-      _helmVars="--set pingfederate-admin.envs.PF_ADMIN_PROFILE_SHA=${pingfederate_adminSha} --set pingfederate-admin.envs.PF_PROFILE_SHA=${pingfederateSha}"
-      timeout="1m30s"
-      ;;
-    pingfederate)
-      _helmVars="--set pingfederate-engine.envs.PF_PROFILE_SHA=${pingfederateSha}"
-      timeout="2m0s"
-      ;;
-    *)
-      timeout="2m0s"
-      ;;
-  esac
-
-  helm upgrade --install \
-    "${PING_ENV}" pingidentity/ping-devops \
-    ${_helmVars} \
-    --set global.envs.SERVER_PROFILE_BRANCH="${_ref}" \
-    -f helm/dev-values.yaml \
-    --force --atomic --timeout "${timeout}"
-done
-
+helm upgrade --install \
+  "${PING_ENV}" pingidentity/ping-devops \
+  --set pingdirectory.envs.SERVER_PROFILE_SHA="${pingdirectorySha}" \
+  --set pingfederate-admin.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
+  --set pingfederate-engine.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
+  --set global.envs.SERVER_PROFILE_BRANCH="${_ref}" \
+  -f helm/dev-values.yaml \
+  --force --atomic --timeout "${_timeout}"
 
 test "${?}" -ne 0 && exit 1
 
