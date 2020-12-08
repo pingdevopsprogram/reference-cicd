ac0eda601f483c9677ad0b9a7a1ea1008a1839cc
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-12-08 15:57:43 -0600

    more helm vars

--- ci-scripts/helm-deploy.sh
+++ ci-scripts/helm-deploy.sh
@@ -18,16 +18,17 @@ for D in ./profiles/* ; do
 done
 
 #try to minimize extended crashloops
-_timeout="2m0s"
+_timeout="4m0s"
 test "${pingdirectorySha}" = "${_currentSha}" && _timeout="10m0s"
 
 # # install the new profiles, but don't move on until install is successfully deployed. 
 # # tied to chart version to avoid breaking changes.
 helm upgrade --install \
   "${PING_ENV}" pingidentity/ping-devops \
-  --set pingdirectory.envs.SERVER_PROFILE_SHA="${pingdirectorySha}" \
-  --set pingfederate-admin.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
-  --set pingfederate-engine.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
+  --set pingdirectory.envs.PD_PROFILE_SHA="${pingdirectorySha}" \
+  --set pingfederate-admin.envs.PF_PROFILE_SHA="${pingfederateSha}" \
+  --set pingfederate-admin.envs.PF_ADMIN_PROFILE_SHA="${pingfederate_adminSha}" \
+  --set pingfederate-engine.envs.PF_PROFILE_SHA="${pingfederateSha}" \
   --set global.envs.SERVER_PROFILE_BRANCH="${_ref}" \
   -f helm/dev-values.yaml \
   --force --atomic --timeout "${_timeout}"
