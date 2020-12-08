ee9d7d4808cc84fb51f8030757ec6ec33a6af91f
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-12-08 14:58:20 -0600

    working on modular helm command

--- ci-scripts/helm-deploy.sh
+++ ci-scripts/helm-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-set -x
+set -x -e
 
 _ref=$(git rev-parse --abbrev-ref HEAD)
 PING_ENV=${PING_ENV:=sg-dev-fs}
@@ -18,13 +18,36 @@ done
 # # install the new profiles, but don't move on until install is successfully deployed. 
 # # tied to chart version to avoid breaking changes.
 ##TODO - turn this into a loop, 1 deploy for each product, so it's easier to rollback failures.  
-helm upgrade --install \
-  "${PING_ENV}" pingidentity/ping-devops \
-  --set pingfederate-admin.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
-  --set pingfederate-engine.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
-  --set pingdirectory.envs.SERVER_PROFILE_SHA="${pingdirectorySha}" \
-  --set global.envs.SERVER_PROFILE_BRANCH="${_ref}" \
-  -f helm/dev-values.yaml \
-  --atomic --timeout 10m0s
+
+for _prodName in  pingdirectory pingfederate_admin pingfederate; do
+  echo "$_prodName Sha is:" "$(eval "echo \${${_prodName}Sha}")"
+  case ${_prodName} in
+    pingdirectory)
+      timeout="10m0s"
+      _helmVars='--set pingdirectory.envs.PD_PROFILE_SHA="$(eval "echo \${${product}sha}")"'
+      ;;
+    pingfederate_admin)
+      _helmVars='--set pingfederate-admin.envs.PF_ADMIN_PROFILE_SHA="${pingfederate_adminSha} --set pingfederate-admin.envs.PF_PROFILE_SHA="${pingfederateSha}'
+      timeout="1m30s"
+      ;;
+    pingfederate)
+      _helmVars='--set pingfederate-engine.envs.PF_PROFILE_SHA="${pingfederateSha}'
+      timeout="2m0s"
+      ;;
+    *)
+      timeout="2m0s"
+      ;;
+  esac
+
+  helm upgrade --install \
+    "${PING_ENV}" pingidentity/ping-devops \
+    "${_helmVars}" \
+    --set global.envs.SERVER_PROFILE_BRANCH="${_ref}" \
+    -f helm/dev-values.yaml \
+    --force --atomic --timeout "${timeout}"
+done
+
+
+test "${?}" -ne 0 && exit 1
 
 helm history "${PING_ENV}"
