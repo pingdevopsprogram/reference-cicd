428e2d7467ac380dfd7931ae2e3b10af3880176d
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-12-07 23:07:47 -0600

    prep ping-env as env var

--- ci-scripts/helm-deploy.sh
+++ ci-scripts/helm-deploy.sh
@@ -3,6 +3,7 @@
 set -x
 
 _ref=$(git rev-parse --abbrev-ref HEAD)
+PING_ENV=${PING_ENV:=sg-dev-fs}
 echo _ref
 
 ## builds sha for each product based on the folder name in ./profiles/* (e.g. pingfederateSha)
@@ -18,7 +19,7 @@ done
 # # tied to chart version to avoid breaking changes.
 ##TODO - turn this into a loop, 1 deploy for each product, so it's easier to rollback failures.  
 helm upgrade --install \
-  "${_pingEnv}" pingidentity/ping-devops \
+  "${PING_ENV}" pingidentity/ping-devops \
   --set pingfederate-admin.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
   --set pingfederate-engine.envs.SERVER_PROFILE_SHA="${pingfederateSha}" \
   --set pingdirectory.envs.SERVER_PROFILE_SHA="${pingdirectorySha}" \
@@ -26,4 +27,4 @@ helm upgrade --install \
   -f helm/dev-values.yaml \
   --atomic --timeout 10m0s
 
-helm history "${_pingEnv}"
+helm history "${PING_ENV}"
