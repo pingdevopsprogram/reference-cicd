bf9b31c9722ccdbc9b943e8f91a6fe8f417d94df
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-12-16 14:24:03 -0600

    make postman tests release specific

--- ci_tools/run_postman.sh
+++ ci_tools/run_postman.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+set -x
+collectionId=08a31b94a3b9fd831861
+test -n "${1}" && collectionId=${1}
 set -a 
 # shellcheck source=./ci_tools.lib.sh
 . ./ci_tools/ci_tools.lib.sh
@@ -6,11 +9,11 @@ set +a
 
 kubectl delete pod integration-tests --force --grace-period=0 --ignore-not-found -n "${K8S_NAMESPACE}"
 
-kubectl get cm "${RELEASE}global-env-vars" -o=jsonpath='{.data}' -n "${K8S_NAMESPACE}" | jq -r '. | to_entries | .[] | .key + "=" + .value + ""' > env_vars
+kubectl get cm "${RELEASE}-global-env-vars" -o=jsonpath='{.data}' -n "${K8S_NAMESPACE}" | jq -r '. | to_entries | .[] | .key + "=" + .value + ""' > env_vars
 postman_vars=$(while read line; do echo "--env-var $line "; done < env_vars)
 
 set -x 
 kubectl run integration-tests \
 -i --rm --restart=Never \
 --image=postman/newman \
--- run https://www.getpostman.com/collections/08a31b94a3b9fd831861 --insecure --ignore-redirects ${postman_vars}
+-- run https://www.getpostman.com/collections/${collectionId} --insecure --ignore-redirects ${postman_vars}
