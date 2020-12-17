dd4c8e0c6fe7f8a6b3faf8be8482e9dcd3b36950
samirgandhi19 <17574913+samir-gandhi@users.noreply.github.com>
2020-12-17 09:42:22 -0600

    small fixes

--- ci_tools/ci_tools.lib.sh
+++ ci_tools/ci_tools.lib.sh
@@ -20,7 +20,12 @@ VALUES_FILE=${VALUES_FILE:=helm/values.yaml}
 CHART_VERSION="0.3.7"
 CURRENT_SHA=$(git log -n 1 --pretty=format:%h)
 
+getGlobalVars() {
+  kubectl get cm "${RELEASE}-global-env-vars" -o=jsonpath='{.data}' -n "${K8S_NAMESPACE}" | jq -r '. | to_entries | .[] | .key + "=" + .value + ""'
+}
+
 getPfVars() {
 export PF_ADMIN_PUBLIC_HOSTNAME=$(kubectl get ing -l app.kubernetes.io/instance=${RELEASE},app.kubernetes.io/name=pingfederate-admin -o=jsonpath='{.items[0].spec.rules[0].host}')
 PING_IDENTITY_PASSWORD="2FederateM0re"
 }
+
--- ci_tools/run_postman.sh
+++ ci_tools/run_postman.sh
@@ -9,11 +9,11 @@ set +a
 
 kubectl delete pod integration-tests --force --grace-period=0 --ignore-not-found -n "${K8S_NAMESPACE}"
 
-kubectl get cm "${RELEASE}-global-env-vars" -o=jsonpath='{.data}' -n "${K8S_NAMESPACE}" | jq -r '. | to_entries | .[] | .key + "=" + .value + ""' > env_vars
-postman_vars=$(while read line; do echo "--env-var $line "; done < env_vars)
+getGlobalVars > postman_vars
+postmanVars=$(while read line; do echo "--env-var $line "; done < postman_vars)
 
 set -x 
 kubectl run integration-tests \
 -i --rm --restart=Never \
 --image=postman/newman \
--- run https://www.getpostman.com/collections/${collectionId} --insecure --ignore-redirects ${postman_vars}
+-- run https://www.getpostman.com/collections/${collectionId} --insecure --ignore-redirects ${postmanVars}
